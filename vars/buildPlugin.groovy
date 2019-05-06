b4c8110975f8b5366a00fe80fe86d6fa2e592d41
Umesh Kumar <mrumeshkumar@hotmail.com>
2019-05-06 13:46:30 +0530

    Adding new files

---
+++ vars/buildPlugin.groovy
@@ -0,0 +1,8 @@
+// vars/buildPlugin.groovy
+def call(Map config) {
+    node {
+        git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
+        sh 'mvn install'
+        mail to: '...', subject: "${config.name} plugin build", body: '...'
+    }
+}
